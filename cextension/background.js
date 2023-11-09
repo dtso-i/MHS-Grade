ad3a3976f1666f7d0bce4b6215d97d805d5345b3
Charlie Becker <charliebecker@charliebecker.local>
2023-11-08 18:13:13 -0800

    auto-progress-save(aps)

--- cextension/background.js
+++ cextension/background.js
@@ -1,5 +1,22 @@
+//sends getHTML on tab update
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete' && tab.url && tab.url.startsWith('https://maranathahighschool.myschoolapp.com/app/student#studentmyday/progress')) {
+    chrome.tabs.sendMessage(tabId, { action: 'getHTML' });
+  }
+});
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "openNewTab") {
     chrome.tabs.create({ url: "http://localhost:8000" });
   }
+  if (message.action === 'htmlResponse') {
+    const filteredHTML = processHTML(message.html);
+  }
 });
+
+function processHTML(html){
+  console.log
+  //filter the html
+  //store data
+  //make another html
+}
--- cextension/popup.js
+++ cextension/popup.js
@@ -1,7 +1,6 @@
-//open localhost:8000 on click
+//send message to open localhost:8000 on click
 document.addEventListener("DOMContentLoaded", function () {
   const openNewTabButton = document.getElementById("popoutButton");
-
   openNewTabButton.addEventListener("click", function () {
     chrome.scripting.executeScript({
       target: { tabId: (tabId) => tabId},
@@ -9,8 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
 function openNewTab() {
   chrome.runtime.sendMessage({ action: "openNewTab" });
 }
 
+//get html on message_received === getHTML
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === 'getHTML') {
+    const html = document.documentElement.outerHTML;
+    chrome.runtime.sendMessage({ action: 'htmlResponse', html: html });
+  }
+});
