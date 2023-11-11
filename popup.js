68902f1e697c50df9c038d9974da930c28aba078
Charlie Becker <charliebecker@charliebecker.local>
2023-11-10 17:02:23 -0800

    mtu

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("changeColorButton").addEventListener("click", () => {
-  chrome.scripting.executeScript({
-    target: { tabId: 0 },
-    function: (color) => {
-      document.body.style.backgroundColor = color;
-    },
-    args: ["blue"], // Change the color to your preference
-  });
-});
